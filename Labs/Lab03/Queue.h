46dccb231ef3508bca2c3382682b78e60d2278d6
ReidConsole <jermainereid@mec.science>
2026-03-27 18:13:25 -0400

    Labs Update

---
+++ Labs/Lab03/Queue.h
@@ -0,0 +1,14 @@
+#ifndef QUEUE_H
+#define QUEUE_H
+
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <stdexcept>
+#include "Node.h"
+
+namespace osl
+{
+}
+
+#endif
---
+++ Labs/Lab03/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <iomanip>
+#include "Node.h"
+#include "Process.h"
+#include "Queue.h"
+
+int main()
+{
+	return 0;
+}
